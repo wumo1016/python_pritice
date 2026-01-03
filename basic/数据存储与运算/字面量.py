d50694f677e48805daf53e8722a9082cd4b857a2
wumo1016 <760478279@qq.com>
2026-01-03 11:22:58 +0800

    feat: 字面量

---
+++ basic/数据存储与运算/字面量.py
@@ -0,0 +1,11 @@
+"""
+- 简介
+  - 字面量是指在代码中直接出现的固定值，例如整数、浮点数、字符串、布尔值等。
+"""
+
+print(123)  # 整数
+print(1.25)  # 浮点数
+print("hello world")  # 字符串
+print(True)  # 布尔值
+print(False)  # 布尔值
+print(None)  # None 表示空值
