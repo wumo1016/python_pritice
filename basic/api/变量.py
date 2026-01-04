3877de0767142c6ff17332799ce228b1a1d489dc
wumo1016 <760478279@qq.com>
2026-01-04 20:15:37 +0800

    chore: 变量

---
+++ basic/api/变量.py
@@ -0,0 +1,47 @@
+"""
+- 简介
+  - 变量是指在程序运行过程中可以改变其值的量。
+"""
+
+
+def f1():
+    s1 = 123
+    print(s1)
+    s1 = "hello world"
+    print(s1)
+
+
+# f1()
+
+
+# 一次性定义多个变量
+def f2():
+    s1, s2 = 123, "hello world"
+    print(s1)
+    print(s2)
+
+
+# f2()
+
+# 查看变量的类型 type isinstance
+def f3():
+    s0 = 123
+    print(type(s0))  # <class 'int'>
+    print(isinstance(s0, int))  # True
+
+    s1 = 1.25
+    print(type(s1))  # <class 'float'>
+    print(isinstance(s1, float))  # True
+
+    s2 = "hello world"
+    print(type(s2))  # <class 'str'>
+    print(isinstance(s2, str))  # True
+
+    s3 = True
+    print(type(s3))  # <class 'bool'>
+    print(isinstance(s3, bool))  # True
+
+    s4 = None
+    print(type(s4))  # <class 'NoneType'>
+
+f3()
