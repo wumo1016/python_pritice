e111955394141e76430b0c9122d0f53200dc3d8a
wumo1016 <760478279@qq.com>
2026-01-10 17:09:46 +0800

    feat: 函数-变量作用域

--- basic/api/函数.py
+++ basic/api/函数.py
@@ -6,7 +6,9 @@
 - 调用
   - 函数名(参数1, 参数2, ...)
 - 函数的说明文档
-- help(函数名): 查看函数的说明文档
+  - help(函数名): 查看函数的说明文档
+- global
+  - global 变量名: 声明变量为全局变量
 
 """
 
@@ -22,4 +24,30 @@ def circle_area_len(r):
     return 2 * 3.14 * r
 
 
-circle_area_len(5)
+# circle_area_len(5)
+
+""" ------------------------ 3.13 函数变量作用域 ------------------------ """
+
+num = 10
+
+
+def f1():
+
+    num = 100
+    print("f1-num1:", num)  # 100
+
+
+# f1()
+# print("global-num:", num)  # 10
+
+
+def f2():
+    # 表示要在函数中使用全局变量 num
+    global num
+    num = 100
+    print("f2-num:", num)  # 100
+
+
+# f2()
+
+# print("global-num:", num)  # 100
